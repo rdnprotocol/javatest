bb3eb1671c03336b56e5459a69019433a30cc109
24LP8224 24LP8224 <24LP8224@G301-CC0200.local>
2024-06-04 12:28:07 +0800

    js

--- index.js
+++ index.js
@@ -46,10 +46,10 @@
 
 // console.log(numberof);
 
-const a = [
-  5, 18, 123, 186, 215, 368, 403, 405, 617, 665, 738, 741, 800, 977, 1109, 1147,
-  1150, 1260, 1304, 1465, 1524, 1729, 1869,
-];
+// const a = [
+//   5, 18, 123, 186, 215, 368, 403, 405, 617, 665, 738, 741, 800, 977, 1109, 1147,
+//   1150, 1260, 1304, 1465, 1524, 1729, 1869,
+// ];
 
 // function max(arr) {
 //   let maxNum = 0;
@@ -76,39 +76,272 @@ const a = [
 // const result = max(a);
 
 // console.log(result);
-const man = "fgaga";
+// const man = "fgaga";
 
-let person = {
-  id: 1234,
-  firstName: "Jack",
-  lastName: "Sprout",
-  email: "jack.sprout@gmail.com",
-  phone: "800-555-1212",
-  type: ["ffff", 2],
-  type2: {
-    mobile: "88076625",
-    utas: 5,
-    utas: 4,
-  },
-  type2: {
-    mobile: "88076625",
-    utas: 5,
-    utas: 3,
-  },
-  photoUrl: "http://www.somesite.com/images/avatar.jpg",
-  type2: {
-    mobile: "88076625",
-    utas: 5,
-    utas: 2,
-  },
+// let person = {
+//   id: 1234,
+//   firstName: "Jack",
+//   lastName: "Sprout",
+//   email: "jack.sprout@gmail.com",
+//   phone: "800-555-1212",
+//   type: ["ffff", 2],
+//   type2: {
+//     mobile: "88076625",
+//     utas: 5,
+//     utas: 4,
+//   },
+//   type2: {
+//     mobile: "88076625",
+//     utas: 5,
+//     utas: 3,
+//   },
+//   photoUrl: "http://www.somesite.com/images/avatar.jpg",
+//   type2: {
+//     mobile: "88076625",
+//     utas: 5,
+//     utas: 2,
+//   },
+// };
+
+// console.log(
+//   `${person.email} ${person.type[0]} ${
+//     (person.type2.mobile, person.type2.utas)
+//   }  hafhahff`
+// );
+
+// const countOccurrence = (arr) => {
+//   let myArr = {};
+
+//   for (i = 0; i < arr.length; i++) {
+//     let num = arr[i];
+//     if (myArr[num] !== undefined) {
+//       myArr[num]++;
+//     } else {
+//       myArr[num] = 1;
+//     }
+//   }
+
+//   return myArr;
+// };
+
+// console.log(
+//   countOccurrence([6, 6, 6, 6, 6, 2, 6, 2, 6, 26, 26, 2, 62, 6, 26, 26, 2])
+// );
+// // for (key = 0; key < person.lenght; key++) {
+// //   console.log(person[key]);
+// // }
+
+// const ArrayOne = [6, 6, 6, 6, 6, 2, -1, 2, 6, 26, 26, 0, 62, 6, 26, 26, 3];
+// // Өгөгдсөн массивын гишүүдийн нийлбэрийг ол.
+// const Total = (arr) => {
+//   numTotal = 0;
+
+//   for (i = 0; i < arr.length; i++) {
+//     numTotal = numTotal + arr[i];
+//   }
+//   return numTotal;
+// };
+
+// console.log(`Өгөгдсөн массивын гишүүдийн нийлбэр ${Total(ArrayOne)}`);
+
+// // Өгөгдсөн массивын 0-ээс их элементүүдийн нийлбэрийг ол.
+// const TotalNotZero = (arr) => {
+//   numTotal = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       numTotal = numTotal + arr[i];
+//     }
+//   }
+//   return numTotal;
+// };
+
+// console.log(
+//   `Өгөгдсөн массивын 0-ээс их элементүүдийн нийлбэр ${TotalNotZero(ArrayOne)} `
+// );
+// // Өгөгдсөн массивын хамгийн бага элементийг ол.
+
+// const MinNum = (arr) => {
+//   let minNum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < minNum) {
+//       minNum = arr[i];
+//     }
+//   }
+//   return minNum;
+// };
+
+// console.log(`Өгөгдсөн массивын хамгийн бага элемент ${MinNum(ArrayOne)} `);
+// // Өгөгдсөн массивын хамгийн их элемент хэд дэх нь вэ? Хэрэв хамгийн их элементийн тоо 1-ээс олон бол бага дугаарыг нь хэвлэнэ.
+// const max = (arr) => {
+//   let maxNum = {
+//     max: 0,
+//     index: 0,
+//   };
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > maxNum.max) {
+//       // console.log(arr[i], i)
+//       maxNum["max"] = arr[i];
+//       maxNum["index"] = i;
+//     }
+//   }
+//   return maxNum;
+// };
+
+// console.log(
+//   `Өгөгдсөн массивын хамгийн их элемент хэд дэх нь вэ? Хэрэв хамгийн их элементийн тоо 1-ээс олон бол бага дугаарыг нь хэвлэнэ ${
+//     max([6, 6, 6, 6, 6, 2, -1, 2, 6, 26, 26, 0, 62, 6, 26, 26, 3]).max
+//   }
+//    ${max([6, 6, 6, 6, 6, 2, -1, 2, 6, 26, 26, 0, 62, 6, 26, 26, 3]).index} `
+// );
+
+// 1. Өгөгдсөн Массивийн элэментүүдийг эсрэг дарааллаар буцаа
+// const reverseArr = (arr) => {
+//   let reverse = [];
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     reverse.push(arr[i]);
+//   }
+//   return reverse;
+// };
+
+// console.log(reverseArr([6, 26, 26, 0, 62, 6, 26, 26, 3]));
+// // 2. Хөрш элэментүүдээсээ их буюу орой элэментүүдийн тоог буцаа
+
+// const peakElements = (arr) => {
+//   let count = 0;
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+//     }
+//     count++;
+
+//     if (arr[0] > arr[1]) {
+//       count++;
+//     }
+//     if (arr[arr.length - 1] > arr[arr.length - 2]) {
+//       count++;
+//     }
+//     return count;
+//   }
+// };
+
+// console.log(peakElements([6, 26, 65, 2]));
+
+// // 3. Массив болон тоо өгөгдөв. Нийлбэр нь өгөгдсөн тоотой тэнцүү байдаг хосын тоог ол
+
+// const printPairs = (arr, target) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === target) {
+//         console.log(`${arr[i]} + ${arr[j]} = ${target}`);
+//       }
+//     }
+//   }
+// };
+
+// printPairs([5, 2, 6, 7, 1], 8);
+// 4. Өгөгдсөн 2 массивийн огтлолцлыг ол
+
+// function findIntersection(arr1, arr2) {
+//   const intersection = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     const element = arr1[i];
+//     if (arr2.indexOf(element) !== -1 && intersection.indexOf(element) === -1) {
+//       intersection.push(element);
+//     }
+//   }
+//   return intersection;
+// }
+
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [3, 4, 5, 6];
+// const intersection = findIntersection(arr1, arr2);
+// console.log(intersection);
+
+// 5. Өгөгдсөн массивийн сөрөг тоонуудыг зүүн талд нь байрлуул
+
+const toLeft = (arr) => {
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      let t = arr[j];
+      arr[j] = arr[i];
+      arr[i] = t;
+      j++;
+    }
+  }
+  return arr;
+};
+console.log(toLeft([6, 26, -65, -2]));
+
+// 6. Дараалсан тоонуудаас бүрдэх массив өгөгдөх байсан боловч 1 тоо нь дутуу байв. Тэр тоог ол
+const FindNum = (arrNum) => {
+  MaxNum = Math.max(...arrNum);
+  MinNum = Math.min(...arrNum);
+  totalNum = ((MinNum + MaxNum) * (arrNum.length + 1)) / 2;
+  totalArr = 0;
+  for (let i = 0; i < arrNum.length; i++) {
+    totalArr += arrNum[i];
+  }
+  let foundNum = totalNum - totalArr;
+  return foundNum;
 };
 
-console.log(
-  `${person.email} ${person.type[0]} ${
-    (person.type2.mobile, person.type2.utas)
-  }  hafhahff`
-);
+console.log(FindNum([1, 2, 3, 5]));
 
-// for (key = 0; key < person.lenght; key++) {
-//   console.log(person[key]);
-// }
+// 1. Массив болон тоо өгөгдөв. Өгөгдсөн тоо массив байгаа бол дугаарыг нь, байхгүй бол -1 буцаа
+// 2. Өгөгдсөн массивын элементүүдийг хойш нь нэг цикл шилжилт хий. Өөрөөр хэлбэл 1-рийг 2-рт, 2-рыг 3-рт, гэх мэт сүүлийн элементийг 1-рт тус тус шилжүүл.
+//     Жич: 1 2 3 4 5 -> 5 1 2 3 4
+// 3. Өгөгдсөн массив өсөх эрэмбээр байвал true үгүй бол false буцаа
+// 4.
+//     HackerLand University has the following grading policy:
+//         - Every student receives a  in the inclusive range from  to .
+//         - Any  less than  is a failing grade.
+//     Sam is a professor at the university and likes to round each student's  according to these rules:
+//     If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+//     If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+//     Examples:
+//     84 - round to  (85 - 84 is less than 3)
+//     28 - do not round (result is less than 40)
+//     57 - do not round (60 - 57 is 3 or higher)
+//     Given the initial value of  for each of Sam's  students, write code to automate the rounding process.
+
+// 1. Массив болон тоо өгөгдөв. Өгөгдсөн тоо массив байгаа бол дугаарыг нь, байхгүй бол -1 буцаа
+const backMyNumber = (arr, num) => {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(backMyNumber([1, 2, 4, 4, 5], 6));
+
+// 2. Өгөгдсөн массивын элементүүдийг хойш нь нэг цикл шилжилт хий. Өөрөөр хэлбэл 1-рийг 2-рт, 2-рыг 3-рт, гэх мэт сүүлийн элементийг 1-рт тус тус шилжүүл.
+//     Жич: 1 2 3 4 5 -> 5 1 2 3 4
+
+// const toStep = (arr) => {
+//   let j = 0;
+//   for (let i = arr.length - 2; i >= 0; i--) {
+//     let t = arr[i + 1];
+//     arr[i + 1] = arr[i];
+//     arr[i] = t;
+//   }
+//   return arr;
+// };
+// console.log(toStep([6, 26, -65, -2]));
+
+const lastFun = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 38 && arr[i] % 5 >= 3) {
+      let uldegdel = arr[i] % 5;
+      newArr.push(arr[i] - uldegdel + 5);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(lastFun([6, 26, 65, 26, 50, 72, 65, 76, 80, 98]));
---
+++ object.js
@@ -0,0 +1,124 @@
+// function Person(name, age, sex) {
+//   this.name = name;
+//   this.age = age;
+//   this.sex = sex;
+// }
+
+// const rand = new Person("Rand McKinnon", 33, "M");
+// const ken = new Person("Ken Jones", 39, "M");
+
+// function Car(make, model, year, owner) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+//   this.owner = owner;
+// }
+
+// const car1 = new Car("Eagle", "Talon TSi", 1993, rand);
+// const car2 = new Car("Nissan", "300ZX", 1992, ken);
+
+// console.log(car1.owner);
+
+// const findOperator = (phoneNumber) => {
+//   const firstTwo = phoneNumber.slice(0, 2);
+
+//   switch (firstTwo) {
+//     case "95":
+//     case "94":
+//     case "99":
+//       return "Mobicom";
+//     case "88":
+//       return "Unitel";
+//     case "96":
+//       return "Skytel";
+//     case "66":
+//       return "Ondo";
+//     case "98":
+//       return "G-Mobile";
+//     default:
+//       return "Invalid Number";
+//   }
+// };
+
+// console.log(findOperator("98076625"));
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i === 3) {
+//     break;
+//   }
+// }
+
+// const arr = [156, 6, 6, 6, 6, 66];
+
+// for (const el of arr) {
+//   console.log(el);
+// }
+
+// const arr2 = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16],
+// ];
+// const findArr = (arr2) => {
+//   for (const i in arr2) {
+//     console.log(arr2[i][i]);
+//   }
+// };
+// findArr(arr2);
+// let myArr = ["tuul", "erkhes", "egshig"];
+
+// const callback = (arr) => {
+//   arr.push("Munkh");
+// };
+
+// callback(myArr);
+
+// // myArr.forEach((nam) => console.log(nam));
+
+// const useCallback = (callback) => {
+//   callback(myArr);
+// };
+
+// useCallback(callback);
+// console.log(myArr);
+
+// const numArr = [5, 6, 7, 8, 9];
+// const findMax = (arr) => {
+//   maxNum = 0;
+//   arr.forEach((el) => {
+//     if (el > maxNum) maxNum = el;
+//   });
+//   return maxNum;
+// };
+
+// console.log(findMax(numArr));
+
+// const lastFun = (arr) => {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 38 && arr[i] % 5 >= 3) {
+//       let uldegdel = arr[i] % 5;
+//       newArr.push(arr[i] - uldegdel + 5);
+//     } else {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// };
+
+let arr = [6, 26, 65, 26, 50, 72, 65, 78, 80, 98];
+
+let newArr = arr.map((el) => {
+  if (el > 38 && el % 5 >= 3) {
+    let uldegdel = el % 5;
+    return el - uldegdel + 5;
+  }
+  return el;
+});
+
+console.log(newArr);
+
+const filteredArray = arr.filter((el) => el % 2 === 1);
+console.log(filteredArray);
