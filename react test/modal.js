b19fdcbe193d858f97755c9b99216c757c5c4792
24LP8224 24LP8224 <24LP8224@G301-CC0200.local>
2024-06-18 12:45:48 +0800

    todo task

--- react test/modal.js
+++ react test/modal.js
@@ -1,18 +1,37 @@
 const newBtn = document.getElementById("newBtn");
 const closeBtn = document.getElementById("closeBtn");
 const container = document.getElementById("container");
-const cards = document.querySelector(".cards");
+const cards = document.querySelectorAll(".cards .card-container");
+const todo = document.querySelector("#todo > div:not(.title)");
+const inProgress = document.querySelector("#inProgress > div:not(.title)");
+const done = document.querySelector("#done > div:not(.title)");
+const blocked = document.querySelector("#blocked > div:not(.title)");
 const titleInput = document.querySelector("input");
 const descInput = document.querySelector("textarea");
+const checkbox = document.querySelector("#check");
+
+const [priorityInput, statusInput] = document.querySelectorAll("select");
+
 const Form = document.querySelector("form");
+
 let index = 0;
-const cardtemp = (title, description, id) => {
+
+let edittingItem = null;
+const cardtemp = (title, description, id, priority) => {
   return `<div class="card">
-  <div>
+  <button onclick= "changeMode(${id})" class="done">Done</button>
+  <div class="content">
     <h1>${title}</h1>
     <p>${description}</p>
+  
   </div>
-  <button onclick= "deleteItem(${id})" class="delete">устгах</button>
+  
+  <div class="buttons">
+    <button onclick= "editItem(${id})" class="edit">E</button>
+  <button onclick= "deleteItem(${id})" class="delete">D</button>
+  <div class="priority">${priority}</div>
+  </div>
+  
 </div>`;
 };
 
@@ -30,14 +49,45 @@ let data = [];
 //Data gaa setlej bna
 
 const setData = (arr) => {
-  data = arr;
+  data = arr.sort((a, b) => a.priorityIndex - b.priorityIndex);
   render();
 };
-//data renderlej bna
+
 const render = () => {
-  cards.innerHTML = "";
+  cards.forEach((cards) => {
+    cards.innerHTML = "";
+  });
+
   data.forEach((item) => {
-    cards.innerHTML += cardtemp(item.title, item.description, item.id);
+    if (item.statusIndex === 0) {
+      todo.innerHTML += cardtemp(
+        item.title,
+        item.description,
+        item.id,
+        item.priority
+      );
+    } else if (item.statusIndex === 1) {
+      inProgress.innerHTML += cardtemp(
+        item.title,
+        item.description,
+        item.id,
+        item.priority
+      );
+    } else if (item.statusIndex === 2) {
+      done.innerHTML += cardtemp(
+        item.title,
+        item.description,
+        item.id,
+        item.priority
+      );
+    } else {
+      blocked.innerHTML += cardtemp(
+        item.title,
+        item.description,
+        item.id,
+        item.priority
+      );
+    }
   });
 };
 
@@ -45,29 +95,62 @@ Form.addEventListener("submit", (ev) => {
   ev.preventDefault();
   //datagaa awch bna
 
+  // let priorityIndex;
   const title = titleInput.value;
   const desc = descInput.value;
+  const priority = priorityInput.value;
+  const priorityIndex = priorityInput.selectedIndex;
+  const statusIndex = statusInput.selectedIndex;
+  const mydate = new Date();
   const newData = [
     ...data,
     {
       id: index,
       title: title,
       description: desc,
+      priority: priority,
+      priorityIndex: priorityIndex,
+      statusIndex: statusIndex,
+      date: mydate,
     },
   ];
-  index++;
 
+  index++;
   setData(newData);
   container.classList.remove("modal");
-  // deleteBtn.forEach((element) => {
-  //   element.addEventListener("click", () => {
-  //     element.parentElement.remove();
-  //   });
-  // });
+  titleInput.value = "";
+  descInput.value = "";
 });
 render();
 
 const deleteItem = (id) => {
   const newData = [...data].filter((item) => item.id !== id);
   setData(newData);
+};
+
+const changeMode = (id) => {
+  const newData = data.map((el) => {
+    if (el.id === id) {
+      el.statusIndex = 2;
+    }
+    return el;
+  });
+  setData(newData);
+};
+const editItem = (id) => {
+  container.classList.add("modal");
+  titleInput.value = edittingItem.title;
+  descInput.value = edittingItem.description;
+  priorityInput.value = edittingItem.priority;
+  const title = titleInput.value;
+  const desc = descInput.value;
+  const priority = priorityInput.value;
+  const priorityIndex = priorityInput.selectedIndex;
+  const updatedData = data.map((el) => {
+    if (el.id === id) {
+      (el.title = title), (el.description = desc), (el.priority = priority);
+    }
+    return el;
+  });
+  setData(updatedData);
 };
